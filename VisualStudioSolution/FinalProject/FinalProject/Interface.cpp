1ff732e6e3d5dc89cec0546686b1593463b70909
davidhonasan <david_honasan@live.com>
2018-05-29 21:57:39 +0700

    interface

---
+++ VisualStudioSolution/FinalProject/FinalProject/Interface.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+#include "interface.h"
+using namespace std;
+
+string b_horizontal = "";
+
+void header(string title) {
+	for (int i = 0; i < 31; i++) {
+		b_horizontal += B_H;
+	}
+
+	system("CLS");
+	cout << B_TL << b_horizontal.c_str() << B_TR << endl;
+	cout << B_V << "\t      " << title.c_str() << "\t\t" << B_V << endl;
+	cout << B_ML << b_horizontal.c_str() << B_MR << endl;
+	cout << B_V << "\t\t\t\t" <<B_V<< endl;
+}
+
+void menu() {
+	while (true) {
+		header("MENU");
+		system("Color 0E");
+		cout << B_V << "\t1. Select map\t\t" << B_V << endl;
+		cout << B_V << "\t2. List all maps\t" << B_V << endl;
+		cout << B_V << "\t3. Create new map\t" << B_V << endl;
+		cout << B_V << "\t4. Exit\t\t\t" << B_V << endl;
+		cout << B_V << "\t\t\t\t" << B_V << endl;
+		cout << B_BL << b_horizontal.c_str() << B_BR << endl;
+		b_horizontal = "";
+		int x = _getch();
+		switch (x) {
+		case '1':
+			cout << "lol1" << endl;
+			break;
+		case '2':
+			cout << "lol2" << endl;
+			break;
+		case '3':
+			cout << "lol3" << endl;
+			break;
+		case '4':
+			exit(0);
+		}
+	}
+}
---
+++ VisualStudioSolution/FinalProject/FinalProject/interface.h
@@ -0,0 +1,13 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#define B_V		(char)186
+#define B_H		(char)205
+#define B_TL	(char)201
+#define B_TR	(char)187
+#define B_ML	(char)204
+#define B_MR	(char)185
+#define B_BL	(char)200
+#define B_BR	(char)188
+
+#endif
